4a6900af3581eec37127b30a64326bb5bfc169c9
sergip8 <sagp758@gmail.com>
2024-03-04 14:59:41 -0500

    fix filters

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,12 @@ module.exports = {
           port: '',
           pathname: '/**',
         },
+        {
+          protocol: 'http',
+          hostname: 'mangado.sytes.net',
+          port: '',
+          pathname: '/**',
+        },
       ],
     },
 }
